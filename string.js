3a7b206d02124c15f46e364efe112ef7ba3fed12
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-07-19 17:43:09 +0530

    How to set and then get

--- string.js
+++ string.js
@@ -1,11 +1,11 @@
 const client=require('./client');
 
 
-async function init(){
-    const result=await client.get("name:2")
-
+async function init(){ 
+    await client.set("name:1", "Omkar pathak")
+    const result=await client.get("name:1");
     console.log(result);
-
 }
+
 
 init();
