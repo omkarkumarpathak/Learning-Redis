9ab4eb95f6b61d60ae781a6022a70966708c31ef
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-07-21 19:33:41 +0530

    Stream corrected

--- stream.js
+++ stream.js
@@ -1,13 +1,26 @@
 import client from "./client.js";
 
 async function init() {
-    await client.xAdd("mystream", "*", {
-        name: "Omkar",
-        msg: "Hello from Redis stream",
-    });
-    
+
+    await client.xAdd('mystream', '*', 'temp',3);
+            
     const messages=await client.xRange('mystream','-','+');
     console.log(messages);
+
+
+   const response = await client.xRead(
+  {
+    key: 'mystream',
+    id: '0' // start from beginning
+  },
+  {
+    COUNT: 2,
+    BLOCK: 5000
+  }
+);
+    const lenght=await client.xLen('myStream');
+    // console.log(lenght);
+    // console.log(response);
 
 }
 
